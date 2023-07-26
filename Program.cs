a10f69569de536a28e818298a8fa3fc44d63833f
Denis-Moiseev <mrtesla666@gmail.com>
2023-07-26 18:58:08 +0800

    finish

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿int[] GetRandomArray(int Length, int min, int max)
+{
+    int[] array = new int[Length];
+    for (int i = 0; i < Length; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+
+int[] array = GetRandomArray(8, 0, 99);
+Console.Write($"[{String.Join(", ",array)}]");
